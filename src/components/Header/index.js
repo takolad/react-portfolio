1d542cdf6a526d19e117f58229b04b694711c89e
John Hinojosa <hinojohn@ymail.com>
2021-06-21 02:40:57 -0500

    added photo, altered styling on header and card

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
       <span className="navbar-brand ms-3">John Hinojosa's Portfolio</span>
       <a className="navbar-brand" href="/react-portfolio/">
         <span className="navText">Home</span>
       </a>
-      <a className="navbar-brand" href="/react-portfolio/portfolio">
+      <Link className="navbar-brand" to="/react-portfolio/portfolio">
         <span className="navText">Portfolio</span>
-      </a>
+      </Link>
       <a
         className="navbar-brand"
         href="https://github.com/takolad"
