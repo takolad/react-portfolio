246a90fda6c854609168c0bbb1631fded3d0c90e
John Hinojosa <hinojohn@ymail.com>
2021-07-09 12:55:21 -0500

    Altered Route pathing in App.js, redirects user back to portfolio if they manually change url to access a non-existent project

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import Portfolio from "./pages/Portfolio";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Detail from "./pages/Detail";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
+        <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/portfolio/:id" component={Detail} />
         <Route exact path="/contact" component={Contact} />
-        <Route path="/" component={Home} />
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
       </Switch>
       <Footer />
     </Router>
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Redirect } from "react-router-dom";
 import repositories from "../util/repos.json";
 import { Row, Col, Container } from "../components/Grid";
 
@@ -9,36 +9,40 @@ function Detail() {
   const targetRepo = repos.filter(
     (repo) => repo.id === parseInt(location.pathname.slice(-1))
   );
-  let imgAlt = targetRepo[0].name + " screenshot";
-  return (
-    <Container>
-      <Row>
-        <Col size="12">
-          <h3>{targetRepo[0].name}</h3>
-          <div className="project-body">
-            <img alt={imgAlt} src={targetRepo[0].img} />
-          </div>
-          <div className="project-links">
-            {targetRepo[0].deploy_url ? (
-              <a
-                href={targetRepo[0].deploy_url}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Deployed Site
+  try {
+    let imgAlt = targetRepo[0].name + " screenshot";
+    return (
+      <Container>
+        <Row>
+          <Col size="12">
+            <h3>{targetRepo[0].name}</h3>
+            <div className="project-body">
+              <img alt={imgAlt} src={targetRepo[0].img} />
+            </div>
+            <div className="project-links">
+              {targetRepo[0].deploy_url ? (
+                <a
+                  href={targetRepo[0].deploy_url}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Deployed Site
+                </a>
+              ) : (
+                "No deployed link yet available"
+              )}
+              <br />
+              <a href={targetRepo[0].repo_url} target="_blank" rel="noreferrer">
+                Repository Link
               </a>
-            ) : (
-              "No deployed link yet available"
-            )}
-            <br />
-            <a href={targetRepo[0].repo_url} target="_blank" rel="noreferrer">
-              Repository Link
-            </a>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  } catch (err) {
+    return <Redirect to="/portfolio" />;
+  }
 }
 
 export default Detail;
